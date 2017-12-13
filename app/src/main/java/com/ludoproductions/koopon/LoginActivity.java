d9ddeee28ab9a50279d20a1fff5db379f2459b3a
LudoProductions <ludo.productionsmx@gmail.com>
2017-12-12 21:16:37 -0600

    Firebase with facebook added

--- app/src/main/java/com/ludoproductions/koopon/LoginActivity.java
+++ app/src/main/java/com/ludoproductions/koopon/LoginActivity.java
@@ -1,15 +1,25 @@
 package com.ludoproductions.koopon;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthCredential;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Arrays;
 
@@ -17,6 +27,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private LoginButton mLoginbutton;
     private CallbackManager mCallBackManager;
+    private FirebaseAuth mFireBaseAuth;
+    private FirebaseAuth.AuthStateListener mFireBaseListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +42,7 @@ public class LoginActivity extends AppCompatActivity {
         mLoginbutton.registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                goMainScreen();
+                handleFacebookAccessToken(loginResult.getAccessToken());
             }
 
             @Override
@@ -41,6 +53,29 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onError(FacebookException error) {
                 Toast.makeText(getApplicationContext(), R.string.error_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mFireBaseAuth = FirebaseAuth.getInstance();
+        mFireBaseListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = mFireBaseAuth.getCurrentUser();
+                if (user != null){
+                    goMainScreen();
+                }
+            }
+        };
+    }
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mFireBaseAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), R.string.firebase_error, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -54,5 +89,17 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFireBaseAuth.addAuthStateListener(mFireBaseListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mFireBaseAuth.removeAuthStateListener(mFireBaseListener);
     }
 }
